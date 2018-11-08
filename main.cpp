ef1d73901792c34061ba9dac146d94dabb9f9fdb
onigirionta <onigirionta@gmail.com>
2018-11-08 03:35:49 +0300

    added input/output files

--- main.cpp
+++ main.cpp
@@ -1,16 +1,38 @@
 #include "parser.h"
 
 #include <iomanip>
+#include <fstream>
 
-int
-main() {
-    auto xss = go(cin);
-    cout << setprecision(12);
+void gnvoerk(const vector<vector<double>>& xss, ostream& out) {
+    out << setprecision(12);
     for (auto& xs : xss) {
         for (auto x : xs) {
-            cout << x << " ";
+            out << x << " ";
         }
-        cout << "\n";
+        out << "\n";
     }
+}
+
+int
+main(int argc, char* argv[]) {
+    if(argc != 3){
+        cerr << "Usage: " << argv[0] << " in.txt out.txt\n";
+        return 1;
+    }
+
+    ifstream in{argv[1]};
+    if (!in.is_open()) {
+        cerr << "Unable to read input file.\n";
+        return 1;
+    }
+
+    ofstream out{argv[2]};
+    if (!out.is_open()) {
+        cerr << "Unable to write output file.\n";
+        return 1;
+    }
+
+    auto xss = go(in);
+    gnvoerk(xss, out);
     return 0;
 }
